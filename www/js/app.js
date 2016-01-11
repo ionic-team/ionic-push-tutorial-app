64ed644744b14bcec8b4422904dda841d926926d
fuiste <drawncloser@gmail.com>
2016-01-11 10:10:51 -0600

    Rewrite

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core','ngCordova','ionic.service.push', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,6 +20,14 @@ angular.module('starter', ['ionic','ionic.service.core','ngCordova','ionic.servi
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
+
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token) {
+      console.log("Device token:",token.token);
+    });
   });
 })
 
